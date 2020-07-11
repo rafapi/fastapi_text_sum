724a9e25f5c9a5831459924fbd435cf56620b3d1
rafapi <rafa@elementai.com>
2020-07-11 19:48:01 +0100

    Add the summaries and pydantic modules

--- project/app/main.py
+++ project/app/main.py
@@ -1,27 +1,29 @@
-import os
+import logging
 
-from fastapi import FastAPI, Depends
-from tortoise.contrib.fastapi import register_tortoise
-
-from app.config import get_settings, Settings
+from fastapi import FastAPI
 
 from app.api import ping
+from app.db import init_db
+
+
+log = logging.getLogger(__name__)
 
 
 def create_application() -> FastAPI:
     application = FastAPI()
-
-    register_tortoise(
-            application,
-            db_url=os.environ.get("DATABASE_URL"),
-            modules={"models": ["app.models.tortoise"]},
-            generate_schemas=True,
-            add_exception_handlers=True,
-            )
-
     application.include_router(ping.router)
 
     return application
 
 
 app = create_application()
+
+
+@app.on_event("startup")
+async def start_event():
+    log.info("Starting up...")
+
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    log.info("Shutting down...")
